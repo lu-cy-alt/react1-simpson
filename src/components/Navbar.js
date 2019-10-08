19ce1d30a674e8aa672f6b3ff9416ede9cbd7950
Lucie Fontela <luciefontela@gmail.com>
2019-10-08 11:55:55 +0200

    simpson

---
+++ src/components/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './Navbar.css';
+
+
+function Navbar() {
+  return (
+    <div className='App'>
+      <a href ='https://fr.wikipedia.org/wiki/Wikip%C3%A9dia:Accueil_principal'>simpson</a>
+    </div>
+    
+  );
+}
+
+
+export default Navbar;
