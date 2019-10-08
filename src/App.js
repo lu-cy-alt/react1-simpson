7612ecbad89371bf4bfc12ea293b0448a7f35095
Lucie Fontela <luciefontela@gmail.com>
2019-10-08 17:21:03 +0200

    quotecard

--- src/App.js
+++ src/App.js
@@ -7,7 +7,18 @@ function App() {
   return (
     <div className='App'>
       <Navbar /> 
-      <QuoteCard />
+
+      <QuoteCard 
+      quote="I believe the children are the future... Unless we stop them now!"
+      character="Homer Simpson"
+      image= "https://i.ytimg.com/vi/Y_Rh8PKscUE/maxresdefault.jpg"
+      />
+      <QuoteCard
+      quote="Me fail English? That's unpossible"
+      character="Ralph Wiggum"
+      image="https://i.kym-cdn.com/entries/icons/original/000/028/973/ralph.jpg"
+      />
+      <QuoteCard quote="Ah l'alcool, la cause et la solution de tous nos problèmes dans la vie."/>
     </div>
   );
 }
--- src/components/QuoteCard.js
+++ src/components/QuoteCard.js
@@ -3,20 +3,18 @@ import React from 'react';
 import './QuoteCard.css';
 
 
-function QuoteCard() {
+function QuoteCard(props) {
     return (
         <figure class="QuoteCard">
-            <img
-                src="https://images.immediate.co.uk/production/volatile/sites/3/2016/03/Simpsons_g2013_R1_marge-04bf0d1.jpg?quality=90&resize=768,574"
-                alt="Nelson Muntz"></img>        
-            <figcaption>
-                <blockquote>
-                    Shoplifting is a victimless crime, like punching someone in the dark. 
-                </blockquote>        
-                <cite>Marge Simpson</cite>
-            </figcaption>
-        </figure>
-    );                              
+            <img src={props.image} alt={props.character} />
+      <figcaption>
+        <blockquote>{props.quote}</blockquote>
+        <cite>{props.character}</cite>
+      </figcaption>
+    </figure>
+  );
 }
 
+    
 export default QuoteCard;
+
